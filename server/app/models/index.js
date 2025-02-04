3cbb8bf0f8a5180905334950cbb1b6bcba66af5c
Avishka Gihan <87961869+AvishkaGihan@users.noreply.github.com>
2025-02-04 13:53:55 +0530

    enhance database models with detailed documentation and transformation methods

--- server/app/models/index.js
+++ server/app/models/index.js
@@ -1,11 +1,26 @@
 import mongoose from "mongoose";
 import "dotenv/config";
+
 import UserModel from "../models/user.model.js";
 import RoleModel from "../models/role.model.js";
 import PostModel from "../models/post.model.js";
 
+/**
+ * MongoDB connection URL from environment variables
+ * @constant {string}
+ */
 const DB_URL = process.env.DB_URL;
 
+/**
+ * Database configuration object
+ * @namespace
+ * @property {Object} mongoose - Mongoose instance
+ * @property {string} url - MongoDB connection URL
+ * @property {Object} user - User model
+ * @property {Object} role - Role model
+ * @property {Object} post - Post model
+ * @property {Array<string>} ROLES - Available user roles in the system
+ */
 const db = {
   mongoose,
   url: DB_URL,
--- server/app/models/post.model.js
+++ server/app/models/post.model.js
@@ -2,14 +2,33 @@ import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 
+/**
+ * Post Schema
+ * @description Schema definition for the Post model that stores user posts
+ */
 const PostSchema = new Schema(
   {
+    /**
+     * Post title
+     * @type {String}
+     * @description The title of the post
+     */
     title: {
       type: String,
     },
+    /**
+     * Post content
+     * @type {String}
+     * @description The main content/body of the post
+     */
     content: {
       type: String,
     },
+    /**
+     * Post author
+     * @type {String}
+     * @description The user who created the post
+     */
     user: {
       type: String,
     },
@@ -19,6 +38,10 @@ const PostSchema = new Schema(
   }
 );
 
+/**
+ * Transform the document before returning to client
+ * @returns {Object} - Modified post object without __v and with id instead of _id
+ */
 PostSchema.method("toJSON", function () {
   const { __v, _id, ...object } = this.toObject();
   object.id = _id;
