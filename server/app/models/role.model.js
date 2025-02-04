1c0b4d8f45a28558c7bc60e8fe0bfa857a72d592
Avishka Gihan <87961869+AvishkaGihan@users.noreply.github.com>
2025-02-04 13:11:36 +0530

    enhance role model schema with detailed documentation and JSON transformation method

--- server/app/models/role.model.js
+++ server/app/models/role.model.js
@@ -2,8 +2,17 @@ import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 
+/**
+ * Role Schema
+ * @description Schema definition for the Role model that defines user permissions
+ */
 const RoleSchema = new Schema(
   {
+    /**
+     * Role name
+     * @type {String}
+     * @description Defines the name of the role (e.g., 'admin', 'user', etc.)
+     */
     name: {
       type: String,
     },
@@ -13,6 +22,10 @@ const RoleSchema = new Schema(
   }
 );
 
+/**
+ * Transform the document before returning to client
+ * @returns {Object} - Modified role object without __v and with id instead of _id
+ */
 RoleSchema.method("toJSON", function () {
   const { __v, _id, ...object } = this.toObject();
   object.id = _id;
